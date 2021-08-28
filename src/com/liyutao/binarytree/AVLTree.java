a9255f9b31e596db68f0cd7a674a85441c021e8c
李宇涛 <15235372075@163.com>
2021-08-28 11:15:44 +0800

    add:平衡树的验证（根据树深判断）。

---
+++ src/com/liyutao/binarytree/AVLTree.java
@@ -0,0 +1,30 @@
+package com.liyutao.binarytree;
+
+/**
+ * @Author liyutao
+ * @Date 2021/8/28 11:01
+ * @Description 平衡树:任意节点的左右子树的深度相差不超过1。
+ * 平衡树的验证
+ *
+ */
+public class AVLTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    boolean flag = true;
+
+    public int isAVLTree(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int h = isAVLTree(root.left);
+        int h2 = isAVLTree(root.right);
+        if (Math.abs(h - h2) > 1) {
+            flag = false;
+        }
+        return Math.max(h, h2) + 1;
+    }
+
+}
