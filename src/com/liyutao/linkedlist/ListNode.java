fcc46fc32f96d9ac6dec535d55fe96ecb22acdb8
李宇涛 <15235372075@163.com>
2021-09-06 22:05:24 +0800

    add:合并两个链表，递归和非递归版本。

---
+++ src/com/liyutao/linkedlist/ListNode.java
@@ -0,0 +1,17 @@
+package com.liyutao.linkedlist;
+
+/**
+ * @Author liyutao
+ * @Date 2021/9/6 21:52
+ * @Description
+ */
+public class ListNode {
+
+    Integer val;
+
+    ListNode next;
+
+    public ListNode(Integer val) {
+        this.val = val;
+    }
+}
---
+++ src/com/liyutao/linkedlist/ListNodeOper.java
@@ -0,0 +1,55 @@
+package com.liyutao.linkedlist;
+
+/**
+ * @Author liyutao
+ * @Date 2021/9/6 7:54
+ * @Description
+ * 合并两个链表
+ */
+public class ListNodeOper {
+
+    public static void main(String[] args) {
+
+    }
+
+    //合并两个链表递归
+    public ListNode mergeTwoListNodeRec(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoListNodeRec(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListNodeRec(l1, l2.next);
+            return l2;
+        }
+    }
+
+    //合并两个链表
+    public ListNode mergeTwoListNode(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null)
+            return l1 == null ? l2 : l1;
+
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+
+
+}
